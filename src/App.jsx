3524485c548740d9ab1920495a5b45d80dafd8b4
olamidej85 <161619447+olamidej85@users.noreply.github.com>
2024-09-11 19:22:11 -0600

    moved the images file

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className='Body'>
       <div className=' d-flex justify-content-between'>
       <a href="index.html"> 
-            <img src="/images/logo.svg" alt="logo" title="Homepage" class="logo "/>
+      <img src="/images/logo.svg" alt="logo" title="Homepage" class="logo "/>
            </a>
            <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
